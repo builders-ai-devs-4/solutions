27c98b0ae036043f3e0044a0337fe32b6143ebf8
Darek Luczynski <rayzku@gmail.com>
2026-03-30 19:27:53 +0200

    feat; working on api, task file changes adding functions

--- s03e04/resources/preparation.py
+++ s03e04/resources/preparation.py
@@ -6,13 +6,7 @@ from dotenv import load_dotenv
 
 from libs.generic_helpers import save_file
 from libs.tomarkdown import extract_files_from_md
-
-parent_folder_path = Path(__file__).parent.parent
-load_dotenv(parent_folder_path / ".env") 
-sys.path.insert(0, str(parent_folder_path.parent)) 
-sys.path.insert(0, str(Path(__file__).parent)) 
-
-import requests
+from libs.database import Database
 
 parent_folder_path = Path(__file__).parent.parent
 load_dotenv(parent_folder_path / ".env") 
@@ -20,21 +14,29 @@ sys.path.insert(0, str(parent_folder_path.parent))
 sys.path.insert(0, str(Path(__file__).parent)) 
 sys.path.insert(0, str(parent_folder_path)) 
 
+# import requests
+
+# parent_folder_path = Path(__file__).parent.parent
+# load_dotenv(parent_folder_path / ".env") 
+# sys.path.insert(0, str(parent_folder_path.parent)) 
+# sys.path.insert(0, str(Path(__file__).parent)) 
+# sys.path.insert(0, str(parent_folder_path)) 
+
 load_dotenv()
 
-AI_DEVS_SECRET = os.getenv('AI_DEVS_SECRET')
-SOLUTION_URL   = os.getenv('SOLUTION_URL')
-DATA_FOLDER    = os.getenv('DATA_FOLDER')
-TASK_NAME      = os.getenv('TASK_NAME')
-AGENTIC_API_URL = os.getenv('AGENTIC_API_URL')
-CSV_FILES_URL = os.getenv('SOURCE_URL1')
-TASK_DATA_FOLDER_PATH = os.getenv("TASK_DATA_FOLDER_PATH")
-PARENT_FOLDER_PATH = os.getenv("PARENT_FOLDER_PATH")    
-DATA_FOLDER_PATH = os.getenv("DATA_FOLDER_PATH")
-DB_DIR_PATH = os.getenv("DB_DIR_PATH")
-CSVS_DIR_PATH = os.getenv("CSVS_DIR_PATH")
+# AI_DEVS_SECRET = os.getenv('AI_DEVS_SECRET')
+# SOLUTION_URL   = os.getenv('SOLUTION_URL')
+# DATA_FOLDER    = os.getenv('DATA_FOLDER')
+# TASK_NAME      = os.getenv('TASK_NAME')
+# AGENTIC_API_URL = os.getenv('AGENTIC_API_URL')
+# CSV_FILES_URL = os.getenv('SOURCE_URL1')
+# TASK_DATA_FOLDER_PATH = os.getenv("TASK_DATA_FOLDER_PATH")
+# PARENT_FOLDER_PATH = os.getenv("PARENT_FOLDER_PATH")    
+# DATA_FOLDER_PATH = os.getenv("DATA_FOLDER_PATH")
+# DB_DIR_PATH = os.getenv("DB_DIR_PATH")
+# CSVS_DIR_PATH = os.getenv("CSVS_DIR_PATH")
 
-from libs.loggers import LoggerCallbackHandler, agent_logger
+from libs.loggers import agent_logger
 
 MODULE_NAME = "preparation"
 
@@ -52,3 +54,14 @@ def save_extracted_csv_files(csv_files_url: str, csvs_dir_path: Path | str, md: 
         agent_logger.info(f"[{MODULE_NAME}] Downloading {filename} from {file_url}")
         save_file(file_url, csvs_dir_path, override=True)
 
+
+def creating_db(csvs_dir_path: Path | str, db_path: Path | str):
+    with Database(db_path) as db:
+        counts = db.load_csv_dir_multi_table(csvs_dir_path)
+        agent_logger.info(f"Loaded CSV files into database. Row counts: {counts}")
+        agent_logger.info(f"Database tables: {db.tables()}")
+
+        for table in db.tables():
+            agent_logger.info(f"\n-- {table} --")
+            for col in db.schema(table):
+                agent_logger.info(f"  {col['column_name']}")
--- s03e04/task/task.py
+++ s03e04/task/task.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import requests
 
+from resources.preparation import creating_db, save_extracted_csv_files
+
 parent_folder_path = Path(__file__).parent.parent
 load_dotenv(parent_folder_path / ".env") 
 sys.path.insert(0, str(parent_folder_path.parent)) 
@@ -13,7 +15,7 @@ sys.path.insert(0, str(parent_folder_path))
 
 load_dotenv()
 from libs.generic_helpers import save_file
-from libs.tomarkdown import extract_files_from_md, transform_html_to_markdown
+from libs.tomarkdown import transform_html_to_markdown
 from libs.database import Database
 
 AI_DEVS_SECRET = os.getenv('AI_DEVS_SECRET')
@@ -37,49 +39,21 @@ os.environ["CSVS_DIR_PATH"] = str(csvs_dir_path)
 db_dir_path.mkdir(parents=True, exist_ok=True)
 csvs_dir_path.mkdir(parents=True, exist_ok=True)
 db_path = db_dir_path / "negotiations.db"
-
-# os.environ["DB_PATH"] = str(db_path)
+os.environ["DB_PATH"] = str(db_path)
 
 from libs.loggers import LoggerCallbackHandler, agent_logger
-
-# from langfuse import Langfuse, get_client
-
-# Singleton initialization of Langfuse (only once, at startup)
-# This ensures that all agents and tools share the same Langfuse instance and configuration.
-
-def save_extracted_csv_files(csv_files_url: str, csvs_dir_path: Path | str, md: Path | str):
-    md = Path(md)
-    csvs_dir_path = Path(csvs_dir_path)
-    files = extract_files_from_md(
-            md.read_text(encoding='utf-8'),
-            base_url=csv_files_url,
-            extensions=[".csv"]
-        )
-    for file_info in files:
-        file_url = file_info["url"]
-        filename = file_info["name"]
-        agent_logger.info(f"[task] Downloading {filename} from {file_url}")
-        save_file(file_url, csvs_dir_path, override=True)
 
 if __name__ == "__main__":
     
     agent_logger.info(f"[task] Starting task: {TASK_NAME}")
     if not db_path.exists():
+        agent_logger.info(f"[task] Database not found. Creating new database at {db_path}")
         md = transform_html_to_markdown(csvs_dir_path, CSV_FILES_URL)
+        agent_logger.info(f"[task] Extracted markdown saved to {md}")
         save_extracted_csv_files(CSV_FILES_URL, csvs_dir_path, md)
-        
-        with Database(db_path) as db:
-
-            counts = db.load_csv_dir_multi_table(csvs_dir_path)
-
-            # Sprawdź co jest w bazie
-            agent_logger.info(f"Database tables: {db.tables()}")
-
-            for table in db.tables():
-                agent_logger.info(f"\n-- {table} --")
-                for col in db.schema(table):
-                    agent_logger.info(f"  {col['column_name']}: {col['column_type']}")
-
+        agent_logger.info(f"[task] CSV files extracted and saved to {csvs_dir_path}")
+        creating_db(csvs_dir_path, db_path)
+        agent_logger.info(f"[task] Database created at {db_path}")
 
             
   
